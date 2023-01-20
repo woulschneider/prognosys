36c23feb4bcd0bdb98ad58445c7f4137ef2595de
flipt1 <112733259+flipt1@users.noreply.github.com>
2023-01-20 13:28:35 -0300

    Add files via upload
    paulo muuuuuuuuuuuuuzy

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+from models.patients import Base, Patient
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello, Word!'
+    return render_template('index.html')
+
+@app.route('/patients', methods=['GET', 'POST'])
+def patients():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        gender = request.form['gender']
+        dob = request.form['dob']
+        phone = request.form['phone']
+        cpf = request.form['cpf']
+        address = request.form['address']
+        city = request.form['city']
+        cep = request.form['cep']
+        patient = Patient(first_name=first_name, last_name=last_name, gender=gender, date_of_birth=dob, phone_number=phone, cpf=cpf, street_address=address, city=city, cep=cep)
+        db.session.add(patient)
+        db.session.commit()
+    patients = Patient.query.all()
+    return render_template('patients.html', patients=patients)
+
+@app.route('/patient/<int:patient_id>', methods=['GET', 'POST'])
+def patient(patient_id):
+    if request.method == 'POST':
+        if request.form.get('update'):
+            patient = Patient.query.filter_by(id=patient_id).first()
+            patient.first_name = request.form['first_name']
+            patient.last_name = request.form['last_name']
+            patient.address = request.form['address']
+            patient.dob = request.form['dob']
+            patient.phone = request.form['phone']
+            patient.cpf = request.form['cpf']
+            patient.city = request.form['city']
+            patient.cep = request.form['cep']
+            db.session.commit()
+        elif request.form.get('delete'):
+            patient = Patient.query.filter_by(id=patient_id).first()
+            db.session.delete(patient)
+            db.session.commit()
+            return redirect(url_for('patients'))
+    patient = Patient.query.filter_by(id=patient_id).first()
+    return render_template('patient.html', patient=patient)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models/patients.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, Date, TIMESTAMP, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+
+class Patient(db.Model):
+    __tablename__ = 'patients'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    phone_number = Column(String, nullable=False)
+    cpf = Column(String, nullable=False)
+    street_address = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    cep = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    updated_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+
+class Diagnostics(db.Model):
+    __tablename__ = 'diagnostics'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+
